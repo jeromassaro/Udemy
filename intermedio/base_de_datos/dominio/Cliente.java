fd3352735792be221a4fbbb2b89dbcf8d878b892
Jero <jero.massaro2001@gmail.com>
2025-06-23 19:32:59 -0300

    Carpeta dominio de base de datos

---
+++ intermedio/base_de_datos/dominio/Cliente.java
@@ -0,0 +1,72 @@
+package intermedio.base_de_datos.dominio;
+
+import java.util.Objects;
+
+public class Cliente {
+    private int id;
+    private String nombre;
+    private String apellido;
+    private int membresia;
+
+    public Cliente(){}
+
+    public Cliente(int id){
+        this.id = id;
+    }
+    public Cliente(int id,String nombre, String apellido, int membresia) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.membresia = membresia;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getMembresia() {
+        return membresia;
+    }
+
+    public void setMembresia(int membresia) {
+        this.membresia = membresia;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "id=" + id +
+                " nombre='" + nombre + '\'' +
+                ", apellido='" + apellido + '\'' +
+                ", membresia=" + membresia +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Cliente cliente = (Cliente) o;
+        return id == cliente.id && membresia == cliente.membresia && Objects.equals(nombre, cliente.nombre) && Objects.equals(apellido, cliente.apellido);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, nombre, apellido, membresia);
+    }
+}
